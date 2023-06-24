3f0ba8f09a952bc1b42e00e41f4d6fe9fffcad3e
fraronbalsara <32577463+fraronbalsara@users.noreply.github.com>
2023-06-24 18:23:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Commerce-Full-Stack-Web-Application
+ 
---
+++ backend-spring-boot-application/src/test/java/com/fsdgroup11/backendspringbootapplication/BackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdgroup11.backendspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
