9d4a2d944e8033200fc7ed400602c5f9e72af297
fraronbalsara <32577463+fraronbalsara@users.noreply.github.com>
2023-08-15 01:12:20 +0530

    Removed CRUD operations for Cart as they were not being utilized.

--- backend-spring-boot-application/src/main/java/com/fsdgroup11/backendspringbootapplication/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fsdgroup11.backendspringbootapplication.repository;
-
-import com.fsdgroup11.backendspringbootapplication.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-     Cart findByEmail(String email);
-     void deleteByEmail(String email);
-}
